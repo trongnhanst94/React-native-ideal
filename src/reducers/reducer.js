69d3a3ee32c8d1ec0f34de7e0c14cd730d623b9e
EvrestNhan <trongnhanst94@yahoo.com.vn>
2018-10-27 21:14:26 +0700

    update code

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,16 +1,7 @@
-// @flow
 import { combineReducers } from 'redux';
-// import NavigationStateReducer from './navigation';
-// import CalendarReducer from './calendar';
-// import GridReducer from './grid';
-// import GalleryReducer from './gallery';
 import AuthReducer from '../containers/Auth/AuthReducer';
 
 export const allReducers = combineReducers({
-  // navigation: NavigationStateReducer,
-  // calendar: CalendarReducer,
-  // grid: GridReducer,
-  // gallery: GalleryReducer,
 
   auth: AuthReducer,
 });
